25877458d6134b23d1345f4533fdaae923ff2a5a
Francisco Barrento <francisco.barrento@gmail.com>
2025-06-29 12:42:01 +0100

    feat(auth): adds email.verify universal route

--- plugins/tenantforge/security/src/Filament/Pages/Auth/EmailVerification/EmailVerificationPrompt.php
+++ plugins/tenantforge/security/src/Filament/Pages/Auth/EmailVerification/EmailVerificationPrompt.php
@@ -4,11 +4,43 @@ declare(strict_types=1);
 
 namespace TenantForge\Security\Filament\Pages\Auth\EmailVerification;
 
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Pages\Auth\EmailVerification\EmailVerificationPrompt as BaseEmailVerificationPrompt;
 use Livewire\Attributes\Layout;
+
+use function __;
 
 #[Layout('components.layouts.auth')]
 final class EmailVerificationPrompt extends BaseEmailVerificationPrompt
 {
     protected static string $view = 'tenantforge:security::filament.auth.email-verification.email-verification-prompt';
+
+    public function resendNotificationAction(): Action
+    {
+        return Action::make('resendNotification')
+            ->link()
+            ->label(__('filament-panels::pages/auth/email-verification/email-verification-prompt.actions.resend_notification.label').'.')
+            ->action(function (): void {
+                try {
+                    $this->rateLimit(2);
+                } catch (TooManyRequestsException $exception) {
+                    $this->getRateLimitedNotification($exception)?->send();
+
+                    return;
+                }
+
+                // dd($this->getVerifiable());
+
+                $this->getVerifiable()->sendEmailVerificationNotification();
+
+                // $this->sendEmailVerificationNotification($this->getVerifiable());
+
+                Notification::make()
+                    ->title(__('filament-panels::pages/auth/email-verification/email-verification-prompt.notifications.notification_resent.title'))
+                    ->success()
+                    ->send();
+            });
+    }
 }
