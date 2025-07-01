a4ef6cea134033e13af94a205040c10bbf07a3a0
Francisco Barrento <francisco.barrento@gmail.com>
2025-07-01 18:39:24 +0100

    fix(login): fixes utniversan sign-in

--- plugins/tenantforge/security/routes/tenant.php
+++ plugins/tenantforge/security/routes/tenant.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyBySubdomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+Route::middleware([
+    'web',
+    InitializeTenancyBySubdomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {});
