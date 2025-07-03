767de0a0cf490d56586a7865259838571e5b33a6
Francisco Barrento <francisco.barrento@gmail.com>
2025-07-03 22:29:12 +0100

    2 route filamentadminauthemail verificationverify not defined (#5)
    * feat(auth): adds email.verify universal route
    
    * feat(auth): adds email.verify universal route
    
    * fix(login): fixes utniversan sign-in
    
    * feat(security): adds a single login endpoint

--- plugins/tenantforge/security/routes/universal.php
+++ plugins/tenantforge/security/routes/universal.php
@@ -1,9 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Stancl\Tenancy\Middleware\InitializeTenancyBySubdomain;
-use TenantForge\Security\Filament\Pages\Auth\Login;
-
-Route::get('/sign-in', Login::class)
-    ->middleware([InitializeTenancyBySubdomain::class])
-    ->name('login');
